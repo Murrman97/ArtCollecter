ccbb88a27a3442d33f36f9dfd9f2940d7a4c14ad
Jacob Murray <Jacobjm74@icloud.com>
2022-03-10 20:41:55 -0500

    woah there nelly

---
+++ src/components/Preview.js
@@ -0,0 +1,101 @@
+import React, { useState } from "react";
+
+/**
+ * We need to import fetchQueryResultsFromURL since we will sometimes have urls in info.prev and info.next
+ * which are query urls.
+ */
+import { fetchQueryResultsFromURL } from "../api";
+
+const Preview = ({
+  setSearchResults,
+  setFeaturedResult,
+  setIsLoading,
+  searchResults,
+}) => {
+  /**
+   * Destructure setSearchResults, setFeaturedResult, and setIsLoading from props
+   * and also destructure info and records from props.searchResults
+   *
+   
+
+   * You need info, records, setSearchResults, setFeaturedResult, and setIsLoading as available constants
+   */
+  const { info, records } = searchResults;
+  const featuredResult = setFeaturedResult;
+  const isLoading = setIsLoading;
+  // console.log(isLoading, "L");
+  // console.log(featuredResult, "FR");
+  // console.log(records, "rec");
+  /**
+   * Don't touch this function, it's good to go.
+   *
+   * It has to be defined inside the Preview component to have access to setIsLoading, setSearchResults, etc...
+   */
+  async function fetchPage(pageUrl) {
+    setIsLoading(true);
+
+    try {
+      const results = await fetchQueryResultsFromURL(pageUrl);
+      setSearchResults(results);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  return (
+    <aside id="preview">
+      <header className="pagination">
+        {/* This button should be disabled if nothing is set in info.prev, and should call fetchPage with info.prev when clicked */}
+        <button
+          disabled={!info.prev}
+          className="previous"
+          onClick={(event) => {
+            fetchPage(info.prev);
+          }}
+        >
+          Previous
+        </button>
+        {/* This button should be disabled if nothing is set in info.next, and should call fetchPage with info.next when clicked */}
+        <button
+          disabled={!info.next}
+          className="next"
+          onClick={(event) => {
+            fetchPage(info.next);
+          }}
+        >
+          Next
+        </button>
+      </header>
+      <section className="results">
+        {records.map((record, i) => {
+          return (
+            <div
+              key={i}
+              className="object-preview"
+              onClick={(event) => {
+                event.preventDefault();
+                // set the featured result to be this record, using setFeaturedResult
+                setFeaturedResult(record);
+              }}
+            >
+              {
+                // if the record.primaryimageurl exists, show this: <img src={ record.primaryimageurl } alt={ record.description } />, otherwise show nothing
+                record.primaryimageurl ? (
+                  <img src={record.primaryimageurl} alt={record.description} />
+                ) : null
+              }
+              {
+                // if the record.title exists, add this: <h3>{ record.title }</h3>, otherwise show this: <h3>MISSING INFO</h3>
+                record.title ? <h3>{record.title}</h3> : <h3>MISSING INFO</h3>
+              }
+            </div>
+          );
+        })}
+      </section>
+    </aside>
+  );
+};
+
+export default Preview;
---
+++ src/index.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+
+//These imports won't work until you fix ./components/index.js
+import { Feature, Loading, Preview, Search, Title } from "./components";
+
+const App = () => {
+  //We are at the App level component, which is top-most. Any state which needs to be shared between immediate children should
+  //be made here, so create state pairs using useState() for:
+
+  const [searchResults, setSearchResults] = useState({ info: {}, records: [] });
+
+  const [featuredResult, setFeaturedResult] = useState(null);
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  return (
+    <div className="app">
+      <Title />
+      {/* <Search /> needs props for setIsLoading and setSearchResults (trigger <Loading /> on search start/end, and transfer results to preview) */}
+      <Search setIsLoading={setIsLoading} setSearchResults={setSearchResults} />
+      {/* <Preview /> needs props for searchResults, setIsLoading and setSearchResults (clicking prev/next buttons), and setFeaturedResult (clicking a preview) */}
+      <Preview
+        searchResults={searchResults}
+        setIsLoading={setIsLoading}
+        setSearchResults={setSearchResults}
+        setFeaturedResult={setFeaturedResult}
+      />
+      {/* <Feature /> needs props for featuredResult, as well as setIsLoading and setSearchResults (clicking on searchable properties) */}
+      <Feature
+        featuredResult={featuredResult}
+        setIsLoading={setIsLoading}
+        setSearchResult={setSearchResults}
+      />
+      {/* <Loading /> is static, but should only render when isLoading is true */}
+      {/* <Loading /> use a ternary and render null if isLoading is false */}
+    </div>
+  );
+};
+
+/**
+ * Bootstrap the <App /> component into the '#app' element in the DOM,
+ * using ReactDOM.render();
+ */
+// console.log(process.env.API_KEY);
+ReactDOM.render(<App />, document.getElementById("app"));
